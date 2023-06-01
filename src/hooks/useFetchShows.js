250db0ba2360c710f4dfae3380b8c8dce13ea13c
Eto Olei <teneeto@gmail.com>
2023-06-01 07:51:34 +0100

    update@expo48

--- src/hooks/useFetchShows.js
+++ src/hooks/useFetchShows.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const useFetchShows = (path, storageKey) => {
   const [shows, setShows] = useState([]);
